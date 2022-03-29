2b4de09be1a009d42af309e14e61e9e0b9d9f970
Priscilla Kalilombe <priscilla@afroic.com>
2022-03-28 23:02:33 -0500

    Finish implementing react toggle switch

--- main/react-toggle-switch/src/App.js
+++ main/react-toggle-switch/src/App.js
@@ -1,5 +1,22 @@
+import { GlobalStyles } from './styles/Global';
+import { Container, Switch, Slider } from './styles/Styles';
+import React, { useState } from 'react';
+
 function App() {
-  return <></>;
+  const [toggle, setToggle] = useState(false);
+  const onToggle = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <>
+      <GlobalStyles />
+      <Container>
+        <Switch onClick={onToggle} toggle={toggle}>
+          <Slider toggle={toggle}></Slider>
+        </Switch>
+      </Container>
+    </>
+  );
 }
 
 export default App;
